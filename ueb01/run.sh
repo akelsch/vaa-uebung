fdf00e6632cd37dabebc32b4b5e16238aea7d3b4
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-11-21 23:15:16 +0100

    Dynamically adjust neighbor count in Docker script

--- ueb01/run.sh
+++ ueb01/run.sh
@@ -6,10 +6,11 @@ if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
 fi
 
 NODE_COUNT=$1
+NEIGHBOR_COUNT=$((NODE_COUNT - 1))
 
 CMDLINE="tail -q "
 for i in $(seq -w 1 "$NODE_COUNT"); do
-  CMDLINE+=">(ueb01 -f ./testdata/config.csv -id ${i} 2>&1 | sed -e 's/^/[${i}]    /;') "
+  CMDLINE+=">(ueb01 -f ./testdata/config.csv -id ${i} -n ${NEIGHBOR_COUNT} 2>&1 | sed -e 's/^/[${i}]    /;') "
 done
 
 eval "${CMDLINE}"
