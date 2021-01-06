c1062ac4d20efedfc1cf42d658198213ba26aceb
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-01-06 22:40:44 +0100

    Chapter 4 example

--- ueb02/scripts/election.sh
+++ ueb02/scripts/election.sh
@@ -20,3 +20,28 @@ while IFS= read -r port; do
   echo "$port"
   eval "printf 'control_message:{command:START_ELECTION}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost $port"
 done <<<"$ELECTION_PORTS"
+
+# Chapter 4 example
+#eval "printf 'control_message:{command:START_ELECTION}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5003"
+#eval "printf 'control_message:{command:START_ELECTION}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5004"
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5000 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5001 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5002 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5003 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5004 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5005 &&
+#printf 'control_message:{command:EXIT}' | protoc.exe --encode=ueb02.Message Message.proto | nc localhost 5006
+#[p-01] 21:49:10.612381 Received control message: EXIT
+#[p-01] 21:49:10.612381 pred 2
+#[p-02] 21:49:10.645381 Received control message: EXIT
+#[p-02] 21:49:10.645381 pred 3
+#[p-03] 21:49:10.679382 Received control message: EXIT
+#[p-03] 21:49:10.679382 pred 5
+#[p-04] 21:49:10.712381 Received control message: EXIT
+#[p-04] 21:49:10.712381 pred 6
+#[p-05] 21:49:10.746381 Received control message: EXIT
+#[p-05] 21:49:10.746381 pred 6
+#[p-06] 21:49:10.780381 Received control message: EXIT
+#[p-06] 21:49:10.780381 pred 5
+#[p-07] 21:49:10.813381 Received control message: EXIT
+#[p-07] 21:49:10.813381 pred 6
