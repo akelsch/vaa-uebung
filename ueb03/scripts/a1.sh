a51131337c1b69700cb7a43942c281f3cc8c0229
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-01-24 20:21:05 +0100

    Adjust script to use random graph/config

--- ueb03/scripts/a1.sh
+++ ueb03/scripts/a1.sh
@@ -6,16 +6,41 @@ if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
 fi
 
 NODE_COUNT=$1
-NEIGHBOR_COUNT=$((NODE_COUNT - 1))
 
-if ((NEIGHBOR_COUNT == 0)); then
-  NEIGHBOR_COUNT=1
-fi
+GRAPH_FILE="./configs/tmp.gv"
+CONFIG_FILE="./configs/tmp.csv"
 
-# TODO zufälligen Graph generieren
+function graphgen() {
+  EDGE_MIN=$((NODE_COUNT + 1))
+  EDGE_MAX=$(((NODE_COUNT * (NODE_COUNT - 1)) / 2))
+  EDGE_LIMIT=$((EDGE_MIN * 2))
+
+  if ((EDGE_MAX > EDGE_LIMIT)); then
+    EDGE_MAX=$EDGE_LIMIT
+  fi
+
+  EDGE_COUNT=$(shuf -i ${EDGE_MIN}-${EDGE_MAX} -n 1)
+
+  echo "n,m"
+  echo "${NODE_COUNT},${EDGE_COUNT}"
+
+  eval "graphgen.exe -n ${NODE_COUNT} -m ${EDGE_COUNT} > ${GRAPH_FILE}"
+}
+
+function confgen() {
+  true >$CONFIG_FILE
+  for i in $(seq 1 "$NODE_COUNT"); do
+    PORT=$((5000 + i - 1))
+    echo "${i},localhost,${PORT}" >>$CONFIG_FILE
+  done
+}
+
+graphgen && confgen
+
 CMDLINE="tail -q "
 for i in $(seq 1 "$NODE_COUNT"); do
-  CMDLINE+=">(baccount.exe -f ./configs/config.csv -gv ./configs/topology.gv -id ${i} 2>&1) "
+  # CMDLINE+=">(baccount.exe -f ./configs/config.csv -gv ./configs/topology.gv -id ${i} 2>&1) "
+  CMDLINE+=">(baccount.exe -f ${CONFIG_FILE} -gv ${GRAPH_FILE} -id ${i} 2>&1) "
 done
 
 eval "$CMDLINE"
